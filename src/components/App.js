21637b13332824114c1a3cfe97f59e4102b35069
AGNESGITAU <agnesgitau40@gmail.com>
2024-04-26 09:03:46 +0300

    mini-project-App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 import blogData from "../data/blog";
+import About from "./About";
+import Article from "./Article";
+import Header from "./Header";
+import ArticleList from "./ArticleList";
 
 
 console.log(blogData);
